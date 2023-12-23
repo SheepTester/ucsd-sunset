9e70159fd744e7b906e252687d4480a048287868
Sean <ussa.say@gmail.com>
2023-12-22 22:30:49 -0800

    Render grade distribution data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from 'react'
-import { Distributions, parseDistributions } from '../util/parseDistributions'
+import {
+  Distribution,
+  Distributions,
+  distributionId,
+  parseDistributions
+} from '../util/distributions'
+
+const courseCodeComparator = new Intl.Collator('en-US', { numeric: true })
 
 export type AppProps = {
   sourceUrl: string
@@ -9,11 +16,80 @@ export function App ({ sourceUrl }: AppProps) {
   const [distributions, setDistributions] = useState<Distributions>({})
 
   useEffect(() => {
-    fetch(sourceUrl)
+    // TEMP: Currently requests once and uses cache indefinitely. In production,
+    // it should always request a new version
+    caches
+      .open('sunset-cache')
+      .then(cache =>
+        cache
+          .match(sourceUrl)
+          .then(
+            response =>
+              response ??
+              fetch(sourceUrl).then(
+                response => (cache.put(sourceUrl, response.clone()), response)
+              )
+          )
+      )
       .then(r => r.text())
       .then(parseDistributions)
       .then(setDistributions)
   }, [sourceUrl])
 
-  return <pre>{JSON.stringify(distributions, null, 2)}</pre>
+  return (
+    <>
+      <main>
+        {Object.entries(distributions)
+          .sort((a, b) => courseCodeComparator.compare(a[0], b[0]))
+          .map(([course, professors]) => (
+            <article className='course' key={course}>
+              <h2 className='course-code'>{course}</h2>
+              <div className='professors'>
+                {Object.entries(professors)
+                  .sort((a, b) => a[0].localeCompare(b[0]))
+                  .map(([professor, terms]) => {
+                    const [last, first] = professor.split(', ')
+                    return (
+                      <section className='professor' key={professor}>
+                        <h3 className='professor-name'>
+                          {first} <strong>{last}</strong>
+                        </h3>
+                        {Object.entries(terms)
+                          .sort((a, b) => a[0].localeCompare(b[0]))
+                          .map(([term, users]) => {
+                            const frequencies: Record<
+                              string,
+                              { distribution: Distribution; count: number }
+                            > = {}
+                            for (const distribution of Object.values(users)) {
+                              const id = distributionId(distribution)
+                              frequencies[id] ??= { distribution, count: 0 }
+                              frequencies[id].count++
+                            }
+                            return (
+                              <>
+                                <h4 className='term-name'>{term}</h4>
+                                {Object.values(frequencies)
+                                  .sort((a, b) => b.count - a.count)
+                                  .map(({ distribution, count }) => (
+                                    <>
+                                      <p className='contribution-count'>
+                                        Reported by {count} student
+                                        {count === 1 ? '' : 's'}.
+                                      </p>
+                                      <pre>{JSON.stringify(distribution)}</pre>
+                                    </>
+                                  ))}
+                              </>
+                            )
+                          })}
+                      </section>
+                    )
+                  })}
+              </div>
+            </article>
+          ))}
+      </main>
+    </>
+  )
 }
--- src/util/distributions.ts
+++ src/util/distributions.ts
@@ -1,16 +1,30 @@
+export type Distribution = {
+  grade: string
+  count: number
+}[]
+
+/** Returns a unique identifier for the grade distribution. */
+export function distributionId (distribution: Distribution): string {
+  return distribution
+    .map(({ grade, count }) => `${grade}:${count}`)
+    .sort()
+    .join(', ')
+}
+
 export type Distributions = {
   [courseCode: string]: {
     [professor: string]: {
       [term: string]: {
-        [userId: string]: {
-          grade: string
-          count: number
-        }[]
+        [userId: string]: Distribution
       }
     }
   }
 }
 
+/**
+ * Parses a TSV file into a mapping of courses, professors, terms, and
+ * contributors to grade distributions.
+ */
 export function parseDistributions (tsv: string): Distributions {
   const distributions: Distributions = {}
   for (const row of tsv.trim().split(/\r?\n/).slice(1)) {
